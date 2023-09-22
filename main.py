7f756b59bc72768fceea7851b6acae6dd6f9a9d3
Aditya Khera <aditya.khera7@gmail.com>
2023-09-22 03:55:38 +0100

    Added confirmation ticket generation

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class User:
 class Hotel:
     def __init__(self, hotelid):
         self.hotelid = hotelid
+        self.name = df.loc[df['id']== hotelid, 'name'].squeeze()
     def seehotel(self):
         pass
     def bookhotel(self):
@@ -32,17 +33,25 @@ class Pay:
 
 class Confi:
     def __init__(self, customername, hotelobj):
-        pass
+        self.customername = customername
+        self.hotel = hotelobj
     def genticket(self):    
-        pass
+        content = f"""
+        Thank you for your reservation!
+        Information:
+        Name: {self.customername}
+        Hotel: {self.hotel.name}
+        """
+        return content
 
-hotelid = input('Enter hotel ID:')
+print(df)
+hotelid = input('Enter hotel ID: ')
 hotel = Hotel(hotelid)
 
 if hotel.availibility():
     hotel.bookhotel()
     name = input('Enter your name: ')
-    conftick = Confi(name, hotel)
+    conftick = Confi(customername =name, hotelobj = hotel)
     print(conftick.genticket())
 
 else: 
