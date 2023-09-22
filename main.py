38637a12683b111a55ab726f4ebe23017a53302a
Aditya Khera <aditya.khera7@gmail.com>
2023-09-22 02:02:44 +0100

    Initial commit, created classes

---
+++ main.py
@@ -0,0 +1,18 @@
+class User:
+    def fetuser(self):
+        pass
+
+class Hotel:
+    def seehotel(self):
+        pass
+    def bookhotel(self):
+        pass
+
+class Pay:
+    def payhotel(self):
+        pass
+
+
+class Confi:
+    def genticket(self):    
+        pass
