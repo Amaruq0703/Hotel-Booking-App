19f4b25144c6d0d205e9b030d3470ad88db1ba94
Aditya Khera <aditya.khera7@gmail.com>
2023-09-22 03:32:30 +0100

    Implemented bookhotel and availibility methods

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
+import pandas as pd 
+
+df = pd.read_csv('hotels.csv', dtype ={'id':str})
+
 class User:
-    def fetuser(self):
+    def getuser(self):
         pass
 
 class Hotel:
+    def __init__(self, hotelid):
+        self.hotelid = hotelid
     def seehotel(self):
         pass
     def bookhotel(self):
-        pass
+        #Booking hotel by changing availibility to no
+
+        df.loc[df['id'] == self.hotelid, 'available'] = 'no'
+        df.to_csv('hotels.csv', index =False)
+        
+    def availibility(self):
+        # Checking if hotel is availible
+
+        availibility =  df.loc[df['id'] == self.hotelid, 'available'].squeeze()
+        if availibility == 'yes':
+            return True
+        else:
+            return False
 
 class Pay:
     def payhotel(self):
         pass
 
-
 class Confi:
+    def __init__(self, customername, hotelobj):
+        pass
     def genticket(self):    
         pass
+
+hotelid = input('Enter hotel ID:')
+hotel = Hotel(hotelid)
+
+if hotel.availibility():
+    hotel.bookhotel()
+    name = input('Enter your name: ')
+    conftick = Confi(name, hotel)
+    print(conftick.genticket())
+
+else: 
+    print('Hotel is not available')
+
+
+
+
+
